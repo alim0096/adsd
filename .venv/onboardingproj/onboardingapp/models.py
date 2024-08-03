e45118dc6967f9f9be1d2693553d17dcc01952a0
alim0096 <alim0096@student.monash.edu>
2024-08-04 08:30:40 +1000

    update

---
+++ .venv/onboardingproj/onboardingapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class ACCIDENT (models.Model):
+    ACCIDENT_NO = models.CharField(max_length=20)
+    ACCIDENT_DATE = models.DateTimeField()
+    DAY_WEEK_DESC = models.CharField(max_length = 10)
+    NODE_ID = models.CharField(max_length=10)
+    SEVERITY = models.IntegerField()
+
+class NODE (models.Model):
+    ACCIDENT_NO = models.CharField(max_length=20)
+    LGA_NAME = models.CharField(max_length = 20)
+    LATITUDE = models.DecimalField(max_digits=22, decimal_places=16)
+    LONGITUDE = models.DecimalField(max_digits=22, decimal_places=16)
+    POSTCODE_CRASH = models.CharField(max_length=5)
